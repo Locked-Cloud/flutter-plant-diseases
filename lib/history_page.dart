6e32922859a4886dd85cc2f7d7960b7e0d71323d
Ibrahim Ahmed <142117977+Locked-Cloud@users.noreply.github.com>
2024-07-10 04:40:13 +0300

    add anther view in the history page that help to rsee all the history

--- lib/history_page.dart
+++ lib/history_page.dart
@@ -1,17 +1,34 @@
 import 'package:flutter/material.dart';
-import 'dart:io';  // Import dart:io for File handling
+import 'dart:io'; // Import dart:io for File handling
 import 'history_manager.dart';
 import 'history_entry.dart';
 import 'detail_page.dart';
 import 'package:intl/intl.dart';
 
-class HistoryPage extends StatelessWidget {
+class HistoryPage extends StatefulWidget {
+  @override
+  _HistoryPageState createState() => _HistoryPageState();
+}
+
+class _HistoryPageState extends State<HistoryPage> {
+  bool _isGridView = false;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text('Upload History'),
         backgroundColor: Colors.teal,
+        actions: [
+          IconButton(
+            icon: Icon(_isGridView ? Icons.view_list : Icons.grid_view),
+            onPressed: () {
+              setState(() {
+                _isGridView = !_isGridView;
+              });
+            },
+          ),
+        ],
       ),
       body: FutureBuilder<List<HistoryEntry>>(
         future: HistoryManager.getHistory(),
@@ -26,52 +43,101 @@ class HistoryPage extends StatelessWidget {
             List<HistoryEntry> historyEntries = snapshot.data!;
             return Padding(
               padding: const EdgeInsets.all(8.0),
-              child: ListView.builder(
-                itemCount: historyEntries.length,
-                itemBuilder: (context, index) {
-                  HistoryEntry entry = historyEntries[index];
-                  String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(entry.timestamp);
-
-                  return Card(
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(15.0),
-                    ),
-                    margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
-                    child: ListTile(
-                      contentPadding: EdgeInsets.all(10.0),
-                      leading: ClipRRect(
-                        borderRadius: BorderRadius.circular(10.0),
-                        child: Image.file(
-                          File(entry.imagePath),
-                          width: 50,
-                          height: 50,
-                          fit: BoxFit.cover,
-                        ),
+              child: _isGridView
+                  ? GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 1.0,
+                        crossAxisSpacing: 10.0,
+                        mainAxisSpacing: 10.0,
                       ),
-                      title: Text(
-                        'Uploaded at: $formattedDate',
-                        style: TextStyle(fontWeight: FontWeight.bold),
-                      ),
-                      subtitle: Text(
-                        'Tap to view details',
-                        style: TextStyle(color: Colors.grey),
-                      ),
-                      trailing: Icon(Icons.arrow_forward_ios),
-                      onTap: () {
-                        Navigator.push(
-                          context,
-                          MaterialPageRoute(
-                            builder: (context) => DetailPage(
-                              image: File(entry.imagePath),  // Convert imagePath to File
-                              result: entry.response,
+                      itemCount: historyEntries.length,
+                      itemBuilder: (context, index) {
+                        HistoryEntry entry = historyEntries[index];
+                        String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(entry.timestamp);
+                        return GestureDetector(
+                          onTap: () {
+                            Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                builder: (context) => DetailPage(
+                                  image: File(entry.imagePath), // Convert imagePath to File
+                                  result: entry.response,
+                                ),
+                              ),
+                            );
+                          },
+                          child: Card(
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(15.0),
+                            ),
+                            margin: EdgeInsets.all(5.0),
+                            child: GridTile(
+                              footer: Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  'Uploaded at: $formattedDate',
+                                  style: TextStyle(fontWeight: FontWeight.bold),
+                                  textAlign: TextAlign.center,
+                                ),
+                              ),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(15.0),
+                                child: Image.file(
+                                  File(entry.imagePath),
+                                  fit: BoxFit.cover,
+                                ),
+                              ),
                             ),
                           ),
                         );
                       },
+                    )
+                  : ListView.builder(
+                      itemCount: historyEntries.length,
+                      itemBuilder: (context, index) {
+                        HistoryEntry entry = historyEntries[index];
+                        String formattedDate = DateFormat('yyyy-MM-dd – kk:mm').format(entry.timestamp);
+                        return Card(
+                          shape: RoundedRectangleBorder(
+                            borderRadius: BorderRadius.circular(15.0),
+                          ),
+                          margin: EdgeInsets.symmetric(vertical: 10.0, horizontal: 5.0),
+                          child: ListTile(
+                            contentPadding: EdgeInsets.all(10.0),
+                            leading: ClipRRect(
+                              borderRadius: BorderRadius.circular(10.0),
+                              child: Image.file(
+                                File(entry.imagePath),
+                                width: 50,
+                                height: 50,
+                                fit: BoxFit.cover,
+                              ),
+                            ),
+                            title: Text(
+                              'Uploaded at: $formattedDate',
+                              style: TextStyle(fontWeight: FontWeight.bold),
+                            ),
+                            subtitle: Text(
+                              'Tap to view details',
+                              style: TextStyle(color: Colors.grey),
+                            ),
+                            trailing: Icon(Icons.arrow_forward_ios),
+                            onTap: () {
+                              Navigator.push(
+                                context,
+                                MaterialPageRoute(
+                                  builder: (context) => DetailPage(
+                                    image: File(entry.imagePath), // Convert imagePath to File
+                                    result: entry.response,
+                                  ),
+                                ),
+                              );
+                            },
+                          ),
+                        );
+                      },
                     ),
-                  );
-                },
-              ),
             );
           }
         },
